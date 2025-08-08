6ad13017193f01d845b8d269b65f953665b14ea4
Igor <rononi@bk.ru>
2025-08-08 22:34:20 +0300

    Пофикшены Баги,надо изучть login.php,FINAL

--- login.php
+++ login.php
@@ -1,2 +1,20 @@
 <?php
 
+require_once('db.php');
+
+$login = $_POST['login'] ?? '';
+$pass = $_POST['pass'] ?? '';
+
+
+if(empty($login) or empty($pass)){
+    echo "Ошибка: Заполните поля";
+}else{
+    $sql = "SELECT * FROM `Users` WHERE login = '$login' AND passwd = '$pass'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0 ){
+        while($row = $result-> fetch_assoc()){
+            echo "Добро пожаловать" . $row['login'];
+        }
+    }else echo "Нет такого пользователя";
+}
--- register.php
+++ register.php
@@ -8,7 +8,7 @@ $passwd = $_POST['pass'];
 $passwd_conf = $_POST['pass_conf'];
 
 
-$sql = "INSERT INTO `Users` (login,passwd,email) VALUES('$name','$email','$passwd')";
+
 
 if(empty($name) or empty($email) or empty($passwd) or empty($passwd_conf)){
     echo "Ошибка: Заполните все поля";
@@ -16,7 +16,7 @@ if(empty($name) or empty($email) or empty($passwd) or empty($passwd_conf)){
     if($passwd != $passwd_conf){
         echo "Ошибка: Пароли не свопадают";
     }else{
-        $sql = "INSERT INTO `Users` (login,passwd,email) VALUES('$name','$email','$passwd')";
+        $sql = "INSERT INTO `Users` (login,passwd,email) VALUES('$name','$passwd','$email')";
         if($conn -> query($sql)){
             echo "Успешная регистрация";
         }
